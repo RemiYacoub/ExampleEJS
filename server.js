4d9d1d7ff14837f4067dd6dd6ea579cb31ef6861
RémiYACOUB <56682474+RemiYacoub@users.noreply.github.com>
2022-04-05 11:55:20 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fs = require("fs")
+const app = express()
+const port = 3000
+
+// Cette ligne indique le répertoire qui contient
+// les fichiers statiques: html, css, js, images etc.
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    let fake_data = []
+    if (fs.existsSync("data/data.json")) {
+        let rawdata = fs.readFileSync("data/data.json");
+        fake_data = JSON.parse(rawdata)
+    }
+
+    res.render('index', { data: fake_data })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
